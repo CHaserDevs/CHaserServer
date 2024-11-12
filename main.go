e994f478142b9cf38f204c5aa89d9ca62e3e5e8c
kqnade <git@k4na.de>
2024-11-12 22:49:22 +0900

    🌱 Init: Fyne init

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+
+  // "CHaserServer/util"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main(){
+  a := app.New()
+  w := a.NewWindow("CHaserServer")
+
+
+	w.ShowAndRun() 
+}
