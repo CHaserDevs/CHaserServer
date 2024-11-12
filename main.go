201002ed409ae535024d81c4406d901768bbf869
kqnade <git@k4na.de>
2024-11-12 23:04:08 +0900

    ➖ Commentout: Unuse import

--- main.go
+++ main.go
@@ -5,14 +5,13 @@ import (
   // "CHaserServer/util"
 
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
+	// "fyne.io/fyne/v2/container"
+	// "fyne.io/fyne/v2/widget"
 )
 
 func main(){
   a := app.New()
   w := a.NewWindow("CHaserServer")
-
 
 	w.ShowAndRun() 
 }
